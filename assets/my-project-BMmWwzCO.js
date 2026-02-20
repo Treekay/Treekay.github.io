e1767d40fe7adb2bf2139bb37937d8a01291d233
Treekay <Treekay@users.noreply.github.com>
2026-02-20 03:47:43 +0000

    deploy: Treekay/homepage_repo@dac452cc8df7642615ddaba8f559ac2fcb524e24

---
+++ assets/my-project-BMmWwzCO.js
@@ -0,0 +1,12 @@
+const t=`---
+title: My Project
+image: /projects/my-project.jpg
+links:
+  - label: GitHub
+    href: https://github.com/xxx
+  - label: Demo
+    href: https://xxx.com
+---
+
+这里是项目简介（支持 markdown）
+`;export{t as default};
