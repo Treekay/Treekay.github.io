bec43337d2922d98823829520aee6f5d1436c75c
Treekay <Treekay@users.noreply.github.com>
2026-02-20 02:20:45 +0000

    deploy: Treekay/homepage_repo@7d0ff28ed62c01d18e32ea0861cd980179193ebe

---
+++ assets/ai-chatbot-Bkxh2hzy.js
@@ -0,0 +1,8 @@
+const n=`一个基于GPT API的聊天机器人项目
+
+技术：
+
+- React
+- Node.js
+- OpenAI API
+`;export{n as default};
---
+++ assets/languages-FRoGUwfq.js
@@ -0,0 +1,6 @@
+const n=`熟练语言：
+
+- C++
+- Python
+- JavaScript
+`;export{n as default};
