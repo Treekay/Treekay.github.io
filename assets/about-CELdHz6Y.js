c98ceac2f04cc6543ad10ce5ae217d0d08776bcb
Treekay <Treekay@users.noreply.github.com>
2026-02-20 21:30:21 +0000

    deploy: Treekay/homepage_repo@921a63fffb6f7e9f5b4f87445f4ca01809623475

---
+++ assets/about-CELdHz6Y.js
@@ -0,0 +1,2 @@
+const e=`Hi, I'm Shukai Huang, a Master student of Information Technology at the University of Auckland. I have worked for 4 years as a backend developer in the game industry in China, and now I'm looking for new opportunities in software development.
+`;export{e as default};
