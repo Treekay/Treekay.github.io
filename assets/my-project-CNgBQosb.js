a7f0fc27079b358205a8d58f1bbb78070c2549b6
Treekay <Treekay@users.noreply.github.com>
2026-02-21 02:45:26 +0000

    deploy: Treekay/homepage_repo@8a8dc77eee1cf77b94f3c19bbf56e3b87b8f4836

---
+++ assets/my-project-CNgBQosb.js
@@ -0,0 +1,12 @@
+const t=`---
+title: My Project
+image: /public/mylist.jpg
+links:
+  - label: GitHub
+    href: https://github.com/Treekay/UWP/tree/master/MyList
+  # - label: Demo
+  #   href: https://xxx.com
+---
+
+这里是项目简介（支持 markdown）
+`;export{t as default};
